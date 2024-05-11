0037cfb5d1b5b652f237d2ace81c8defd7623ba3
warmcotton <wonseok0807@naver.com>
2024-05-12 03:11:47 +0900

    개발 환경 세팅: LearningLog entity 추가

---
+++ src/main/java/com/thinkbigdata/anna/repository/LearningLogRepository.java
@@ -0,0 +1,7 @@
+package com.thinkbigdata.anna.repository;
+
+import com.thinkbigdata.anna.entity.LearningLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LearningLogRepository extends JpaRepository<LearningLog, Integer> {
+}
