059b489280beae807e4a2cf240c8876fc5b6d24a
warmcotton <wonseok0807@naver.com>
2024-05-12 00:41:02 +0900

    개발 환경 세팅: UserRepositoryTest UserTopic 관련 테스트 추가

--- src/main/java/com/thinkbigdata/anna/repository/UserTopicRepository.java
+++ src/main/java/com/thinkbigdata/anna/repository/UserTopicRepository.java
@@ -1,7 +1,11 @@
 package com.thinkbigdata.anna.repository;
 
+import com.thinkbigdata.anna.entity.User;
 import com.thinkbigdata.anna.entity.UserTopic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserTopicRepository extends JpaRepository<UserTopic, Integer> {
+    List<UserTopic> findByUser(User user);
 }
--- src/test/java/com/thinkbigdata/anna/repository/UserRepositoryTest.java
+++ src/test/java/com/thinkbigdata/anna/repository/UserRepositoryTest.java
@@ -1,7 +1,11 @@
 package com.thinkbigdata.anna.repository;
 
+import com.thinkbigdata.anna.entity.Topic;
 import com.thinkbigdata.anna.entity.User;
+import com.thinkbigdata.anna.entity.UserTopic;
+import com.thinkbigdata.anna.topic.TopicName;
 import org.hibernate.exception.ConstraintViolationException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
@@ -11,6 +15,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -22,7 +29,22 @@ public class UserRepositoryTest {
     @Autowired
     UserRepository userRepository;
     @Autowired
+    TopicRepository topicRepository;
+    @Autowired
+    UserTopicRepository userTopicRepository;
+    @Autowired
     TestEntityManager testEntityManager;
+    void saveTopics() {
+        for (TopicName topicName: TopicName.values()) {
+            Topic topic = new Topic();
+            topic.setTopicName(topicName);
+            topicRepository.save(topic);
+        }
+    }
+    @BeforeEach
+    void contextLoads() {
+        saveTopics();
+    }
 
     @Test
     void save() {
@@ -69,5 +91,38 @@ public class UserRepositoryTest {
         assertThrows(ConstraintViolationException.class, () -> {
             testEntityManager.flush();
         });
+    }
+
+    @Test
+    void save_with_topic() {
+        User user = new User();
+        user.setEmail("test@test.com");
+        user.setPassword("1111");
+        user.setName("Name");
+        user.setNickname("NickName");
+        user.setAge(20);
+        user.setGender("M");
+
+        User savedUser = userRepository.save(user);
+        testEntityManager.flush();
+
+        Topic topic1 = topicRepository.findByTopicName(TopicName.TOPIC1).get();
+        Topic topic2 = topicRepository.findByTopicName(TopicName.TOPIC2).get();
+
+        UserTopic userTopic1 = new UserTopic();
+        userTopic1.setUser(savedUser);
+        userTopic1.setTopic(topic1);
+
+        UserTopic userTopic2 = new UserTopic();
+        userTopic2.setUser(savedUser);
+        userTopic2.setTopic(topic2);
+
+        List<UserTopic> userTopics = asList(userTopic1, userTopic2);
+        userTopicRepository.saveAll(userTopics);
+        testEntityManager.flush();
+
+        List<UserTopic> savedUserTopics = userTopicRepository.findByUser(savedUser);
+
+        assertArrayEquals(savedUserTopics.toArray(), userTopics.toArray());
     }
 }
