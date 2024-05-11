c09db7ffb95ac4ff871ed7cafb112f7a20392a2d
warmcotton <wonseok0807@naver.com>
2024-05-11 18:25:15 +0900

    Initial commit

---
+++ src/main/java/com/thinkbigdata/anna/AnnaApplication.java
@@ -0,0 +1,13 @@
+package com.thinkbigdata.anna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnaApplication.class, args);
+	}
+
+}
